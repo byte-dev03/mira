88f3f18505267feff1c5697d204f128673a55096
byte-dev03 <t63613018@gmail.com>
2024-07-28 21:05:27 +0800

    Initial commit

---
+++ main.lua
@@ -0,0 +1,14 @@
+
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  love.graphics.setBackgroundColor(love.math.colorFromBytes(100, 100, 100, 255))
+  
+end
